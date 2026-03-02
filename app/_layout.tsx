1b88a8fbb388c748319c4a7af54b00255659eeb3
jajaja3000 <jreymon007@gmail.com>
2026-03-02 00:49:42 -0500

    ultimocamnio

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,20 +1,35 @@
 import { AuthProvider, useAuth } from '@/providers/AuthProviders'
-import { Slot } from 'expo-router'
+import { Slot, useRouter, useSegments } from 'expo-router'
+import { useEffect } from 'react'
 import { Text } from "react-native"
 import { GestureHandlerRootView } from 'react-native-gesture-handler'
 
 function RootNavigator() {
   const { session, loading } = useAuth()
+  const segments = useSegments()
+  const router = useRouter()
+
+  useEffect(() => {
+    if (loading) return
+
+    const inAuthGroup = segments[0] === "(auth)"
+
+    if (!session && !inAuthGroup) {
+      router.replace("/(auth)/login")
+    }
+
+    if (session && inAuthGroup) {
+      router.replace("/")
+    }
+  }, [session, loading])
 
   if (loading) {
-    return(
+    return (
       <GestureHandlerRootView style={{flex:1,justifyContent:"center",alignItems:"center"}}>
         <Text>Cargando...</Text>
       </GestureHandlerRootView>
     )
   }
-
-
 
   return <Slot />
 }
