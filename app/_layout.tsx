49215f71d7f9b8c24729fdefdae69e995c760865
jajaja3000 <jreymon007@gmail.com>
2026-03-01 22:39:21 -0500

    menucarga

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,18 @@
 import { AuthProvider, useAuth } from '@/providers/AuthProviders'
 import { Redirect, Slot } from 'expo-router'
+import { Text } from "react-native"
 import { GestureHandlerRootView } from 'react-native-gesture-handler'
 
 function RootNavigator() {
   const { session, loading } = useAuth()
 
-  if (loading) return null
+  if (loading) {
+    return(
+      <GestureHandlerRootView style={{flex:1,justifyContent:"center",alignItems:"center"}}>
+        <Text>Cargando...</Text>
+      </GestureHandlerRootView>
+    )
+  }
 
   if (!session) {
     return <Redirect href="/(auth)/login" />
